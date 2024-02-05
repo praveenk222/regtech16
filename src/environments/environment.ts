cda9e7b3313d3d2369287018db8ae453a104ff73
praveenk222 <praveenkadma222@gmail.com>
2024-02-06 00:57:19 +0530

    d

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  azimageUrl:'https://everdevuat.blob.core.windows.net/',
+  environmentvariable:'test',
+apiurl:'http://localhost:8080/api/v1/adminweb/',
+m_apiurl:'http://localhost:8080/api/',
+// apiurl:'https://everbackend.onrender.com/api/v1/adminweb/',
+
+  // apiurl:'https://172.188.80.209:8443/api/',
+  azimageUrl_hub:'https://everdevuat.blob.core.windows.net/hubs/',
+  azimageUrl_pic:'https://everdevuat.blob.core.windows.net/profilepic/',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
