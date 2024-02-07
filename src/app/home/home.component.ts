6f96de56c8d52a775c2eb00656120a8f2724e60f
nimmalapalli <nimmalapallinarendra@gmail.com>
2024-02-08 00:24:10 +0530

    code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RegisterService } from '../services/register.service';
 import { LocalstorageService } from '../services/localstorage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,8 +11,20 @@ import { LocalstorageService } from '../services/localstorage.service';
 })
 export class HomeComponent {
   user: any;
-  constructor(public nav:RegisterService,private localStorageService:LocalstorageService){
+  constructor(public nav:RegisterService,private localStorageService:LocalstorageService,public ls:LocalstorageService,private router:Router){
     this.nav.show()
   this.user= this.localStorageService.getData('user')
+
+  this.nav.show()
+  this.user= this.ls.getData('user')
+  if(this.user){
+
+    // alert(this.user.IsActive)
+  }
+  }
+  logout(){
+    localStorage.clear();
+    this.router.navigateByUrl('')
+
   }
 }
